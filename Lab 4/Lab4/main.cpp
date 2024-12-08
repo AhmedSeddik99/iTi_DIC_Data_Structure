702433e269f70256e47fcd573a6fab0688f1bf69
AhmedSeddik99 <ahmedseddik2499@gmail.com>
2024-12-08 18:06:02 +0200

    Add files via upload

---
+++ Lab 4/Lab4/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Tree.h"
+using namespace std;
+
+int main()
+{
+    Tree tr;
+
+    tr.add(50);
+    tr.add(60);
+    tr.add(40);
+    tr.add(70);
+    tr.add(55);
+    tr.add(30);
+    tr.add(45);
+    tr.add(35);
+    tr.add(20);
+    tr.add(37);
+
+
+
+    tr.getMaxDepth();
+
+
+    return 0;
+}
